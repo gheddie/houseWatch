266892d4a04054211ad9f7ab9f0241fe89a2b579
stefan.schulz <stefan.schulz@bcc-ws65.bcc.intern>
2013-09-20 10:39:52 +0200

    Nachrichtenverarbeitung per File-System wieder eingebaut

--- housewatch-client/src/main/java/de/gravitex/rp/core/RPConnector.java
+++ housewatch-client/src/main/java/de/gravitex/rp/core/RPConnector.java
@@ -1,15 +1,20 @@
 package de.gravitex.rp.core;
 
+import java.io.File;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import de.gravitex.rp.logic.RPRemote;
 import de.gravitex.rp.logic.WindowStateInfo;
@@ -19,17 +24,53 @@ public class RPConnector {
 	private static final String PROP_WINDOWS_IDENTIFIER = "windowsIdentifier";
 
 	private static final String PROP_NEW_WINDOW_STATE = "newWindowState";
+	
+	//WINDOWS
+	private static final String BASE_DIR = "C:\\Users\\stefan.schulz\\Desktop\\raspberry\\messages";
+	private static final String TARGET_DIR = "C:\\Users\\stefan.schulz\\Desktop\\raspberry\\messages\\processed\\";
+	
+	//LINUX
+//	private static final String BASE_DIR = "/var/tmp/messages/";
+//	private static final String TARGET_DIR = "/var/tmp/messages/processed/";
+	
+	private static final String ATTRIBUTE_IDENTIFIER = "identifier";
+
+	private static final String ATTRIBUTE_STATE = "state";	
 
 	private static final String EJB_MODULE_NAME = "rp-server";
 
 	public static void main(String[] args) throws Exception {
 		
-		Properties properties = parseCommandLine(args);
-		if (properties == null) {			
-			return;
-		}
+		System.out.println("processing messages...");
+		
+//		Properties properties = parseCommandLine(args);
+		
+//		if (properties == null) {			
+//			return;
+//		}
+		
 		RPRemote testRemote = lookup(EJB_MODULE_NAME, "RPBean", RPRemote.class);
-		testRemote.toggleWindowState(properties.getProperty(PROP_WINDOWS_IDENTIFIER), WindowStateInfo.valueOf(properties.getProperty(PROP_NEW_WINDOW_STATE)));
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		String attrIdentifier = null;
+		String attrState = null;
+		Document doc = null;
+		Element root = null;
+		File destination = null;
+		for (File file : new File(BASE_DIR).listFiles()) {
+			if (!(file.isDirectory())) {
+				System.out.println(file.getName());	
+				doc = dBuilder.parse(file);
+				root = doc.getDocumentElement();				
+				attrIdentifier = root.getAttribute(ATTRIBUTE_IDENTIFIER);
+				attrState = root.getAttribute(ATTRIBUTE_STATE);
+				testRemote.toggleWindowState(attrIdentifier, WindowStateInfo.valueOf(attrState));
+				System.out.println("processing message [identifier:"+attrIdentifier+"|state:"+attrState+"]");				
+				destination = new File(TARGET_DIR+file.getName());
+				file.renameTo(destination);
+			}			
+		}
 	}
 	
 	private static Properties parseCommandLine(String[] args) {
